16b1907cf518705c4aa8e164cbf089d78dd666c0
praisesun <blackstar934589@gmail.com>
2024-03-13 15:46:40 +0200

    додаємо шлях

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -1,5 +1,7 @@
 package ua.edu.lntu.cw_3
 
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.res.colorResource
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
