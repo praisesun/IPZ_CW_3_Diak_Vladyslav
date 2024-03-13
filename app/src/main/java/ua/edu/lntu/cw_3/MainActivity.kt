a7cb5f083b2909296facc8de09d9a4513ae6583d
praisesun <blackstar934589@gmail.com>
2024-03-13 15:41:16 +0200

    додаємо відступи

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -15,11 +21,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw_3.ui.theme.PZ_CW_3_Diak_VladyslavTheme
+import kotlin.time.Duration.Companion.days
+
 //test push, initial one doesn't work properly
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +47,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Screen(modifier: Modifier = Modifier) {
+
+    val items = (1..31).toList().map { Post(it) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -44,10 +57,38 @@ fun Screen(modifier: Modifier = Modifier) {
     ) {
         Text(
             text = "30 Days of activities",
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
             style = TextStyle(fontSize = 30.sp, color = Color.Black)
         )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(items) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    shape = RoundedCornerShape(8.dp),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = "Day ${it.days}")
+                        Text(text = it.title, style = TextStyle(fontSize = 24.sp))
+                        Image(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp),
+                            painter = painterResource(id = it.randomImageRes),
+                            contentDescription = ""
+                        )
+                        Text(text = it.description)
+                    }
+                }
+            }
+        }
     }
 }
 
