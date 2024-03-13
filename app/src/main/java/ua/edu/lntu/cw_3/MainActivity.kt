ddeee73f0fe87d8fd2b5d51e57299b9a8828de33
praisesun <blackstar934589@gmail.com>
2024-03-13 14:42:57 +0200

    створюємо головний екран

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -3,26 +3,31 @@ package ua.edu.lntu.cw_3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw_3.ui.theme.PZ_CW_3_Diak_VladyslavTheme
 //test push, initial one doesn't work properly
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             PZ_CW_3_Diak_VladyslavTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Screen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -30,17 +35,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Screen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+
+    }
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "30 Days of different activities",
+        modifier = modifier, style = TextStyle(fontSize = 32.sp, color = Color.Black)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PZ_CW_3_Diak_VladyslavTheme {
-        Greeting("Android")
-    }
+fun ScreenPreview() {
+    PZ_CW_3_Diak_VladyslavTheme { Screen() }
 }
